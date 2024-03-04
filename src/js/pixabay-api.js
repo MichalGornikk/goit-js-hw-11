b67637f8969d1893058ebd36c13a143b94dc1e75
Michał "Bobi" Górnik <64360492+MichalGornikk@users.noreply.github.com>
2024-03-04 21:37:57 +0100

    fixed fails

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,22 +3,48 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 const API_KEY = '42651463-7e9bc4d6a898bed570bd4622e';
 const BaseUrl = 'https://pixabay.com/api/';
-const loader = document.querySelector('.loader');
+
+function trackNetworkRequests() {
+  var originalOpen = XMLHttpRequest.prototype.open;
+  XMLHttpRequest.prototype.open = function () {
+    this.addEventListener('load', function () {
+      if (this.responseURL && this.responseURL.match(/\.(jpeg|jpg|gif|png)$/)) {
+        console.log('Nowy obraz: ' + this.responseURL);
+      }
+    });
+    originalOpen.apply(this, arguments);
+  };
+}
 
 export function searchImages(searchQuery) {
-  // Usuwamy spacje z searchQuery
-  const sanitizedSearchQuery = searchQuery.replace(/\s/g, '');
+  if (searchQuery.trim() === '') {
+    iziToast.error({
+      title: 'Error',
+      fontSize: 'large',
+      close: false,
+      position: 'topRight',
+      messageColor: 'white',
+      timeout: 2000,
+      backgroundColor: 'red',
+      message: 'Please enter some text.',
+    });
+    return Promise.reject('Please enter some text.');
+  }
+
+  const keywords = searchQuery.trim().split(/\s+/);
+  const sanitizedKeywords = keywords.join('+');
 
   const params = new URLSearchParams({
     key: API_KEY,
-    q: sanitizedSearchQuery, // Używamy zsanityzowanej wersji wyszukiwania
+    q: sanitizedKeywords,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
   });
-  loader.style.display = 'block';
 
-  return fetch(`${BaseUrl}?${params}`)
+  trackNetworkRequests();
+
+  return fetch(BaseUrl + '?' + params)
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not OK');
@@ -26,7 +52,6 @@ export function searchImages(searchQuery) {
       return response.json();
     })
     .then(data => {
-      loader.style.display = 'none';
       if (data.hits.length === 0) {
         iziToast.error({
           title: 'Error',
